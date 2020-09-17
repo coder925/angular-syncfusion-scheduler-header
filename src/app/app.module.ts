cb19a09bd0d5ba8f5d58a71bb6e14663b54567c1
Michael Bergman <michael@simutek.se>
2020-09-17 17:08:52 +0200

    Add mat-select.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { SelectResourceComponent } from './select-resource/select-resource.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectResourceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/select-resource/select-resource.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SelectResourceComponent } from './select-resource.component';
+
+describe('SelectResourceComponent', () => {
+  let component: SelectResourceComponent;
+  let fixture: ComponentFixture<SelectResourceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SelectResourceComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SelectResourceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
